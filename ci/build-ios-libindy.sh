221b4767d2140f81eacb49a886a255f0064bbb55
Clécio Varjão <1348549+cvarjao@users.noreply.github.com>
2022-05-15 16:43:17 -0700

    Checkpoint
    Signed-off-by: Clécio Varjão <1348549+cvarjao@users.noreply.github.com>

--- ci/build-ios-libindy.sh
+++ ci/build-ios-libindy.sh
@@ -15,8 +15,22 @@ while [[ $# -gt 0 ]]; do
       shift # past argument
       shift # past value
       ;;
+    --component)
+      if [ "$2" == "core" ]; then
+        BUILD_CORE="true"
+      elif [ "$2" == "framework" ]; then
+        BUILD_FRAMEWORK="true"
+      fi
+      shift # past argument
+      shift # past value
+      ;;
     --clean)
       OPT_CLEAN="true"
+      shift # past argument
+      ;;
+    --all)
+      BUILD_CORE="true"
+      BUILD_FRAMEWORK="true"
       shift # past argument
       ;;
     -*|--*)
@@ -32,6 +46,8 @@ done
 
 set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
 
+
+
 set
 #echo $OPTS
 #exit 
@@ -42,107 +58,115 @@ export PKG_CONFIG_ALLOW_CROSS=1
 
 # change the current directory to the root of the repository
 pushd "${SCRIPT_DIR}/../"
-rustup default "${RUST_VER}"
-rustup target add aarch64-apple-ios armv7-apple-ios armv7s-apple-ios i386-apple-ios x86_64-apple-ios
-if [ "$OPT_CLEAN" == "true" ]; then
-    echo "Cleaning previous build"
-    rm -rf libindy/target
-    rm -rf libindy/out_pod
+if [ "$BUILD_CORE" == "true" ]; then
+    rustup default "${RUST_VER}"
+    rustup target add aarch64-apple-ios armv7-apple-ios armv7s-apple-ios i386-apple-ios x86_64-apple-ios
+    if [ "$OPT_CLEAN" == "true" ]; then
+        echo "Cleaning previous build"
+        rm -rf libindy/target
+        rm -rf libindy/out_pod
+    fi
+
+    if [ ! -d libindy/target ]; then
+        ./ci/ios-build.sh libindy "${_targets}"
+    else
+        echo "Skipping build of libindy (reusing from cache libindy/target)"
+    fi
+    file libindy/out_pod/libindy.a
 fi
 
-if [ ! -d libindy/target ]; then
-    ./ci/ios-build.sh libindy "${_targets}"
-else
-    echo "Skipping build of libindy (reusing from cache libindy/target)"
-fi
+if [ "$BUILD_FRAMEWORK" == "true" ]; then
+    pushd wrappers/ios/libindy-pod
 
-file libindy/out_pod/libindy.a
+    # Indy.xcworkspace
+    if [ ! -d Pods/Pods.xcodeproj ]; then
+        pod install
+    else
+        echo "Skipping 'pod install' (reusing from previous run)"
+    fi
 
-pushd wrappers/ios/libindy-pod
+    BUILD_DIR="$(pwd)/build"
+    FRAMEWORK_DEVICE_PATH='build/iphoneos/Build/Products/Release-iphoneos/Indy.framework'
+    FRAMEWORK_SIMULATOR_PATH='build/iphonesimulator/Build/Products/Release-iphonesimulator/Indy.framework'
 
-# Indy.xcworkspace
-if [ ! -d Pods/Pods.xcodeproj ]; then
-    pod install
-else
-    echo "Skipping 'pod install' (reusing from previous run)"
-fi
+    if [ "$OPT_CLEAN" == "true" ]; then
+      rm -rf "build"
+    fi
 
-BUILD_DIR="$(pwd)/build"
-FRAMEWORK_DEVICE_PATH='build/iphoneos/Build/Products/Release-iphoneos/Indy.framework'
-FRAMEWORK_SIMULATOR_PATH='build/iphonesimulator/Build/Products/Release-iphonesimulator/Indy.framework'
+    mkdir -p build/iphoneos
+    if [ ! -d "${FRAMEWORK_DEVICE_PATH}" ]; then
+        echo "Building for iOS devices"
+        #xcodebuild -workspace Indy.xcworkspace -scheme Indy -configuration Release -sdk iphoneos -archivePath Indy-Device.xcarchive -verbose clean archive
+        xcodebuild -workspace Indy.xcworkspace -scheme Indy -configuration Release -arch arm64 -arch armv7 only_active_arch=no defines_module=yes -sdk "iphoneos" -derivedDataPath "build/iphoneos" clean build BUILD_LIBRARY_FOR_DISTRIBUTION=YES > build/iphoneos/build.log
+    else
+        echo "Skipping build for iOS devices (reusing from previous run)"
+    fi
 
-mkdir -p build/iphoneos
-if [ ! -d "${FRAMEWORK_DEVICE_PATH}" ]; then
-    echo "Building for iOS devices"
-    #xcodebuild -workspace Indy.xcworkspace -scheme Indy -configuration Release -sdk iphoneos -archivePath Indy-Device.xcarchive -verbose clean archive
-    xcodebuild -workspace Indy.xcworkspace -scheme Indy -configuration Release -arch arm64 -arch armv7 only_active_arch=no defines_module=yes -sdk "iphoneos" -derivedDataPath "build/iphoneos" clean build > build/iphoneos/build.log
-else
-    echo "Skipping build for iOS devices (reusing from previous run)"
-fi
+    mkdir -p build/iphonesimulator
+    if [ ! -d "${FRAMEWORK_SIMULATOR_PATH}" ]; then
+        echo "Building for iOS simulators"
+        #xcodebuild -workspace Indy.xcworkspace -scheme Indy -configuration Release -sdk iphonesimulator -archivePath Indy-Simulator.xcarchive -verbose clean archive
+        xcodebuild -workspace Indy.xcworkspace -scheme Indy -configuration Release -arch x86_64 -arch i386 only_active_arch=no defines_module=yes -sdk "iphonesimulator" -derivedDataPath "build/iphonesimulator" clean build BUILD_LIBRARY_FOR_DISTRIBUTION=YES > build/iphonesimulator/build.log
+    else
+        echo "Skipping build for iOS simulators (reusing from previous run)"
+    fi
 
-mkdir -p build/iphonesimulator
-if [ ! -d "${FRAMEWORK_SIMULATOR_PATH}" ]; then
-    echo "Building for iOS simulators"
-    #xcodebuild -workspace Indy.xcworkspace -scheme Indy -configuration Release -sdk iphonesimulator -archivePath Indy-Simulator.xcarchive -verbose clean archive
-    xcodebuild -workspace Indy.xcworkspace -scheme Indy -configuration Release -arch x86_64 -arch i386 only_active_arch=no defines_module=yes -sdk "iphonesimulator" -derivedDataPath "build/iphonesimulator" clean build > build/iphonesimulator/build.log
-else
-    echo "Skipping build for iOS simulators (reusing from previous run)"
-fi
+    FRAMEWORK_UNIVERSAL_PATH='build/universal/Indy.framework'
+    XCFRAMEWORK_UNIVERSAL_PATH='build/universal/Indy.xcframework'
 
+    FRAMEWORK_NAME="Indy"
+    if [ -d "${FRAMEWORK_UNIVERSAL_PATH}" ]; then
+        rm -rf "${FRAMEWORK_UNIVERSAL_PATH}"
+    fi
+    mkdir -p "${FRAMEWORK_UNIVERSAL_PATH}"
 
+    rm -rf "${XCFRAMEWORK_UNIVERSAL_PATH}"
+    xcrun xcodebuild -create-xcframework -framework "${FRAMEWORK_DEVICE_PATH}" -framework "${FRAMEWORK_SIMULATOR_PATH}" -output "${XCFRAMEWORK_UNIVERSAL_PATH}"
 
+    #Copy the device version of framework.
+    cp -r "${FRAMEWORK_DEVICE_PATH}/" "${FRAMEWORK_UNIVERSAL_PATH}"
 
-FRAMEWORK_UNIVERSAL_PATH='build/universal/Indy.framework'
+    ## Merging the device and simulator frameworks' executables with lipo.
+    lipo -create -output "${FRAMEWORK_UNIVERSAL_PATH}/${FRAMEWORK_NAME}" "${FRAMEWORK_DEVICE_PATH}/${FRAMEWORK_NAME}" "${FRAMEWORK_SIMULATOR_PATH}/${FRAMEWORK_NAME}"
+    file "${FRAMEWORK_UNIVERSAL_PATH}/${FRAMEWORK_NAME}"
 
-FRAMEWORK_NAME="Indy"
-if [ -d "${FRAMEWORK_UNIVERSAL_PATH}" ]; then
-    rm -rf "${FRAMEWORK_UNIVERSAL_PATH}"
-fi
-mkdir -p "${FRAMEWORK_UNIVERSAL_PATH}"
+    # Copy Swift module mappings for simulator into the framework. 
+    cp -r "${FRAMEWORK_SIMULATOR_PATH}/Modules/${FRAMEWORK_NAME}.swiftmodule/" "${FRAMEWORK_UNIVERSAL_PATH}/Modules/${FRAMEWORK_NAME}.swiftmodule"
 
-#Copy the device version of framework.
-cp -r "${FRAMEWORK_DEVICE_PATH}/" "${FRAMEWORK_UNIVERSAL_PATH}"
+    # Merge Swift header
+    COMBINED_SWIFT_HEADER_FILE="${FRAMEWORK_UNIVERSAL_PATH}/Headers/${FRAMEWORK_NAME}-Swift.h"
 
-## Merging the device and simulator frameworks' executables with lipo.
-lipo -create -output "${FRAMEWORK_UNIVERSAL_PATH}/${FRAMEWORK_NAME}" "${FRAMEWORK_DEVICE_PATH}/${FRAMEWORK_NAME}" "${FRAMEWORK_SIMULATOR_PATH}/${FRAMEWORK_NAME}"
-file "${FRAMEWORK_UNIVERSAL_PATH}/${FRAMEWORK_NAME}"
-
-# Copy Swift module mappings for simulator into the framework. 
-cp -r "${FRAMEWORK_SIMULATOR_PATH}/Modules/${FRAMEWORK_NAME}.swiftmodule/" "${FRAMEWORK_UNIVERSAL_PATH}/Modules/${FRAMEWORK_NAME}.swiftmodule"
-
-# Merge Swift header
-COMBINED_SWIFT_HEADER_FILE="${FRAMEWORK_UNIVERSAL_PATH}/Headers/${FRAMEWORK_NAME}-Swift.h"
-
-touch "${COMBINED_SWIFT_HEADER_FILE}"
-echo "#ifndef TARGET_OS_SIMULATOR" > "${COMBINED_SWIFT_HEADER_FILE}"
-echo "#include <TargetConditionals.h>" >> "${COMBINED_SWIFT_HEADER_FILE}"
-echo "#endif" >> "${COMBINED_SWIFT_HEADER_FILE}"
-echo "#if TARGET_OS_SIMULATOR" >> "${COMBINED_SWIFT_HEADER_FILE}"
-cat "${FRAMEWORK_SIMULATOR_PATH}/Headers/${FRAMEWORK_NAME}-Swift.h" >> "${COMBINED_SWIFT_HEADER_FILE}"
-echo "#else" >> "${COMBINED_SWIFT_HEADER_FILE}"
-echo "//Start of iphoneos" >> "${COMBINED_SWIFT_HEADER_FILE}"
-cat "${FRAMEWORK_DEVICE_PATH}/Headers/${FRAMEWORK_NAME}-Swift.h" >> "${COMBINED_SWIFT_HEADER_FILE}"
-echo "#endif" >> "${COMBINED_SWIFT_HEADER_FILE}"
+    touch "${COMBINED_SWIFT_HEADER_FILE}"
+    echo "#ifndef TARGET_OS_SIMULATOR" > "${COMBINED_SWIFT_HEADER_FILE}"
+    echo "#include <TargetConditionals.h>" >> "${COMBINED_SWIFT_HEADER_FILE}"
+    echo "#endif" >> "${COMBINED_SWIFT_HEADER_FILE}"
+    echo "#if TARGET_OS_SIMULATOR" >> "${COMBINED_SWIFT_HEADER_FILE}"
+    cat "${FRAMEWORK_SIMULATOR_PATH}/Headers/${FRAMEWORK_NAME}-Swift.h" >> "${COMBINED_SWIFT_HEADER_FILE}"
+    echo "#else" >> "${COMBINED_SWIFT_HEADER_FILE}"
+    echo "//Start of iphoneos" >> "${COMBINED_SWIFT_HEADER_FILE}"
+    cat "${FRAMEWORK_DEVICE_PATH}/Headers/${FRAMEWORK_NAME}-Swift.h" >> "${COMBINED_SWIFT_HEADER_FILE}"
+    echo "#endif" >> "${COMBINED_SWIFT_HEADER_FILE}"
 
 
 
-# xcrun xcodebuild -create-xcframework -framework /path/to/ios.framework -framework /path/to/sim.framework -output combined.xcframework
+    # xcrun xcodebuild -create-xcframework -framework /path/to/ios.framework -framework /path/to/sim.framework -output combined.xcframework
 
-mkdir -p "${BUILD_DIR}/cocoapods/build/Frameworks"
+    rm -rf "${BUILD_DIR}/cocoapods"
+    mkdir -p "${BUILD_DIR}/cocoapods/build/Frameworks"
 
-pushd "${BUILD_DIR}/cocoapods/build/Frameworks/"
-ln -sf '../../../iphonesimulator/Build/Products/Release-iphonesimulator/Indy.framework'
-#zip -r "${BUILD_DIR}/libindy-objc.zip" "Indy.framework"
-#tar -cvzf "${BUILD_DIR}/libindy-objc.tar.gz" "Indy.framework"
-popd # dirname "${FRAMEWORK_SIMULATOR_PATH}"
-pushd "${BUILD_DIR}/cocoapods"
-zip -r libindy-objc.zip "build"
-popd
+    pushd "${BUILD_DIR}/cocoapods/build/Frameworks/"
+    #ln -sf '../../../iphonesimulator/Build/Products/Release-iphonesimulator/Indy.framework'
+    ln -sf '../../../universal/Indy.xcframework'
+    #zip -r "${BUILD_DIR}/libindy-objc.zip" "Indy.framework"
+    #tar -cvzf "${BUILD_DIR}/libindy-objc.tar.gz" "Indy.framework"
+    popd # dirname "${FRAMEWORK_SIMULATOR_PATH}"
+    pushd "${BUILD_DIR}/cocoapods"
+    zip -r libindy-objc.zip "build"
+    popd
 
-#rm -rf "${HOME}/Documents/GitHub/bc-wallet-mobile/app/ios/Pods/Frameworks/Indy.framework"
-#mkdir -p "${HOME}/Documents/GitHub/bc-wallet-mobile/app/ios/Pods/Frameworks"
-#cp -r "${FRAMEWORK_SIMULATOR_PATH}" "${HOME}/Documents/GitHub/bc-wallet-mobile/app/ios/Pods/Frameworks/"
-
-
-popd # wrappers/ios/libindy-pod
+    #rm -rf "${HOME}/Documents/GitHub/bc-wallet-mobile/app/ios/Pods/Frameworks/Indy.framework"
+    #mkdir -p "${HOME}/Documents/GitHub/bc-wallet-mobile/app/ios/Pods/Frameworks"
+    #cp -r "${FRAMEWORK_SIMULATOR_PATH}" "${HOME}/Documents/GitHub/bc-wallet-mobile/app/ios/Pods/Frameworks/"
+    popd # wrappers/ios/libindy-pod
+fi 
 popd # <root>
